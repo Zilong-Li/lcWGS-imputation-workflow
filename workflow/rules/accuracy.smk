6ac6a7efd0a08d422ed7affa068b375589dcb612
Zilong-Li <zimusen94@gmail.com>
2022-09-19 13:12:18 +0100

    add glimpse in accuracy

--- workflow/rules/accuracy.smk
+++ workflow/rules/accuracy.smk
@@ -1,14 +1,33 @@
 
-rule collect_imputed_gts:
+rule collect_truth_gts:
+    output:
+        os.path.join(
+            OUTDIR_SUMMARY,
+            "truth.gts.{chrom}.panelsize{size}.down{depth}x.{chrom}.txt",
+        ),
+    log:
+        os.path.join(
+            OUTDIR_SUMMARY,
+            "truth.gts.{chrom}.panelsize{size}.down{depth}x.{chrom}.log",
+        ),
+    params:
+        samples=",".join(SAMPLES.keys()),
+        truth=lambda wildcards: REFPANEL[wildcards.chrom]["truth"],
+        ql1="%CHROM:%POS:%REF:%ALT\\t%AF\\t[\\t%GT]\\n",
+    conda:
+        "../envs/quilt.yaml"
+    shell:
+        """
+        bcftools +fill-tags {params.truth} -- -t AF |  bcftools view -s {params.samples} | bcftools query -f '{params.ql1}' | sed -E 's/\/|\|/\\t/g' > {output}
+        """
+
+
+rule collect_quilt_imputed_gts:
     input:
         regular=rules.quilt_ligate_regular.output.vcf,
         mspbwt=rules.quilt_ligate_mspbwt.output.vcf,
         zilong=rules.quilt_ligate_zilong.output.vcf,
     output:
-        truth=os.path.join(
-            OUTDIR_SUMMARY,
-            "truth.gts.{chrom}.panelsize{size}.down{depth}x.{chrom}.txt",
-        ),
         regular=os.path.join(
             OUTDIR_SUMMARY,
             "quilt.gts.regular.panelsize{size}.down{depth}x.{chrom}.txt",
@@ -21,43 +40,63 @@ rule collect_imputed_gts:
         ),
     log:
         os.path.join(
-            OUTDIR_SUMMARY, "accuracy.panelsize{size}.down{depth}x.{chrom}.llog"
+            OUTDIR_SUMMARY, "quilt.gts.panelsize{size}.down{depth}x.{chrom}.llog"
         ),
     params:
         samples=",".join(SAMPLES.keys()),
-        truth=lambda wildcards: REFPANEL[wildcards.chrom]["truth"],
-        ql1="%CHROM:%POS:%REF:%ALT\\t%AF\\t[\\t%GT]\\n",
         ql2="%CHROM:%POS:%REF:%ALT\\t[\\t%GT\\t%DS]\\n",
     conda:
         "../envs/quilt.yaml"
     shell:
         """
-        bcftools +fill-tags {params.truth} -- -t AF |  bcftools view -s {params.samples} | bcftools query -f '{params.ql1}' | sed -E 's/\/|\|/\\t/g' > {output.truth}
         bcftools query -f '{params.ql2}' -s {params.samples} {input.regular} | sed -E 's/\/|\|/\\t/g' > {output.regular}
         bcftools query -f '{params.ql2}' -s {params.samples} {input.mspbwt} | sed -E 's/\/|\|/\\t/g' > {output.mspbwt}
         bcftools query -f '{params.ql2}' -s {params.samples} {input.zilong} | sed -E 's/\/|\|/\\t/g' > {output.zilong}
         """
 
 
-rule plot_accuracy:
+rule collect_glimpse_imputed_gts:
+    input:
+        rules.glimpse_ligate.output.vcf,
+    output:
+        os.path.join(
+            OUTDIR_SUMMARY,
+            "glimpse.gts.panelsize{size}.down{depth}x.{chrom}.txt",
+        ),
+    log:
+        os.path.join(
+            OUTDIR_SUMMARY, "glimpse.gts.panelsize{size}.down{depth}x.{chrom}.llog"
+        ),
+    params:
+        samples=",".join(SAMPLES.keys()),
+        ql2="%CHROM:%POS:%REF:%ALT\\t[\\t%GT\\t%DS]\\n",
+    conda:
+        "../envs/quilt.yaml"
+    shell:
+        """
+        bcftools query -f '{params.ql2}' -s {params.samples} {input} | sed -E 's/\/|\|/\\t/g' > {output}
+        """
+
+
+rule plot_quilt_accuracy:
     input:
         truth=expand(
-            rules.collect_imputed_gts.output.truth,
+            rules.collect_truth_gts.output,
             depth=config["downsample"],
             allow_missing=True,
         ),
         regular=expand(
-            rules.collect_imputed_gts.output.regular,
+            rules.collect_quilt_imputed_gts.output.regular,
             depth=config["downsample"],
             allow_missing=True,
         ),
         mspbwt=expand(
-            rules.collect_imputed_gts.output.mspbwt,
+            rules.collect_quilt_imputed_gts.output.mspbwt,
             depth=config["downsample"],
             allow_missing=True,
         ),
         zilong=expand(
-            rules.collect_imputed_gts.output.zilong,
+            rules.collect_quilt_imputed_gts.output.zilong,
             depth=config["downsample"],
             allow_missing=True,
         ),
@@ -69,3 +108,40 @@ rule plot_accuracy:
         "../envs/quilt.yaml"
     script:
         "../scripts/accuracy.R"
+
+
+rule plot_all_accuracy:
+    input:
+        truth=expand(
+            rules.collect_truth_gts.output,
+            depth=config["downsample"],
+            allow_missing=True,
+        ),
+        glimpse=expand(
+            rules.collect_glimpse_imputed_gts.output,
+            depth=config["downsample"],
+            allow_missing=True,
+        ),
+        regular=expand(
+            rules.collect_quilt_imputed_gts.output.regular,
+            depth=config["downsample"],
+            allow_missing=True,
+        ),
+        mspbwt=expand(
+            rules.collect_quilt_imputed_gts.output.mspbwt,
+            depth=config["downsample"],
+            allow_missing=True,
+        ),
+        zilong=expand(
+            rules.collect_quilt_imputed_gts.output.zilong,
+            depth=config["downsample"],
+            allow_missing=True,
+        ),
+    output:
+        os.path.join(OUTDIR_SUMMARY, "all.accuracy.panelsize{size}.{chrom}.pdf"),
+    log:
+        os.path.join(OUTDIR_SUMMARY, "all.accuracy.panelsize{size}.{chrom}.pdf.llog"),
+    conda:
+        "../envs/quilt.yaml"
+    script:
+        "../scripts/accuracy2.R"
---
+++ workflow/scripts/accuracy2.R
@@ -0,0 +1,94 @@
+acc_r2_all <- function(d0, d1, d2, d3, d4) {
+    truthGT <- as.vector(sapply(seq(1, dim(d0)[2] - 1, 2), function(i){rowSums(d0[,(i+1):(i+2)])}))
+    idx <- seq(1, dim(d1)[2], 3) # dosage indicies
+    y1 <- cor(truthGT, as.vector(as.matrix(d1[, idx[-1]])), use = 'pairwise.complete') ** 2
+    idx <- seq(1, dim(d2)[2], 3) # dosage indicies
+    y2 <- cor(truthGT, as.vector(as.matrix(d2[, idx[-1]])), use = 'pairwise.complete') ** 2
+    idx <- seq(1, dim(d3)[2], 3) # dosage indicies
+    y3 <- cor(truthGT, as.vector(as.matrix(d3[, idx[-1]])), use = 'pairwise.complete') ** 2
+    idx <- seq(1, dim(d4)[2], 3) # dosage indicies
+    y4 <- cor(truthGT, as.vector(as.matrix(d4[, idx[-1]])), use = 'pairwise.complete') ** 2
+    c(y1,y2,y3,y4)
+}
+
+acc_r2_by_af <- function(d0, d1, d2, d3, d4, af, breaks) {
+    truthGT <- sapply(seq(1, dim(d0)[2] - 1, 2), function(i){rowSums(d0[,(i+1):(i+2)])})  # matrix: nsnps x nsamples
+    idx <- seq(1, dim(d1)[2], 3) # dosage indicies
+    d1 <- as.matrix(d1[, idx[-1]])
+    x <- cut(af, breaks = breaks)
+    d1_cor_af <- tapply(1:length(x), x, function(w) { c(n = length(w),
+                                                        nA = sum(truthGT[w,], na.rm = TRUE),
+                                                        simple = cor(as.vector(truthGT[w,]), as.vector(d1[w,]), use = 'pairwise.complete') ** 2
+                                                        )})
+    idx <- seq(1, dim(d2)[2], 3) # dosage indicies
+    d2 <- as.matrix(d2[, idx[-1]])
+    d2_cor_af <- tapply(1:length(x), x, function(w) { c(n = length(w),
+                                                        nA = sum(truthGT[w,], na.rm = TRUE),
+                                                        simple = cor(as.vector(truthGT[w,]), as.vector(d2[w,]), use = 'pairwise.complete') ** 2
+                                                        )})
+    idx <- seq(1, dim(d3)[2], 3) # dosage indicies
+    d3 <- as.matrix(d3[, idx[-1]])
+    d3_cor_af <- tapply(1:length(x), x, function(w) { c(n = length(w),
+                                                        nA = sum(truthGT[w,], na.rm = TRUE),
+                                                        simple = cor(as.vector(truthGT[w,]), as.vector(d3[w,]), use = 'pairwise.complete') ** 2
+                                                        )})
+    idx <- seq(1, dim(d4)[2], 3) # dosage indicies
+    d4 <- as.matrix(d4[, idx[-1]])
+    d4_cor_af <- tapply(1:length(x), x, function(w) { c(n = length(w),
+                                                        nA = sum(truthGT[w,], na.rm = TRUE),
+                                                        simple = cor(as.vector(truthGT[w,]), as.vector(d4[w,]), use = 'pairwise.complete') ** 2
+                                                        )})
+    as.data.frame(cbind(bin = breaks[-1], regular = sapply(d1_cor_af, "[[", "simple"),  mspbwt = sapply(d2_cor_af, "[[", "simple"), zilong = sapply(d3_cor_af, "[[", "simple"), glimpse = sapply(d4_cor_af, "[[", "simple")))
+}
+
+deps <- as.numeric(snakemake@config[["downsample"]])
+
+df.truth <- read.table(snakemake@input[["truth"]][[1]])
+af <- as.numeric(df.truth[,2])
+af <- ifelse(af>0.5, 1-af, af)
+df.truth <- df.truth[,-2] # remove af column now
+
+dl.regular <- lapply(snakemake@input[["regular"]], read.table)
+dl.mspbwt <- lapply(snakemake@input[["mspbwt"]], read.table)
+dl.zilong <- lapply(snakemake@input[["zilong"]], read.table)
+dl.glimpse <- lapply(snakemake@input[["glimpse"]], read.table)
+
+bins <- sort(unique(c(
+    c(0, 0.01 / 10, 0.02 / 10, 0.05 / 10),
+    c(0, 0.01 / 1, 0.02 / 1, 0.05 / 1),
+    seq(0.1, 0.5, length.out = 5)
+)))
+
+accuracy <- matrix(sapply(1:length(deps), function(i) {
+    acc_r2_all(df.truth, dl.regular[[i]], dl.mspbwt[[i]], dl.zilong[[i]], dl.glimpse[[i]])
+}), ncol = length(deps))
+
+
+accuracy_by_af <- lapply(1:length(deps), function(i) {
+    acc_r2_by_af(df.truth, dl.regular[[i]], dl.mspbwt[[i]], dl.zilong[[i]], dl.glimpse[[i]], af, bins)
+})
+
+pdf(snakemake@output[[1]], w=12, h=6)
+par(mfrow = c(1, 2))
+plot(deps, accuracy[1,], type = "b", lwd=1.5, pch = 1, ylab = "Aggregated R2 for the chromosome", xlab = "Samples sequencing depth", ylim=c(0.4, 1.0))
+lines(deps, accuracy[2,], type = "b", lwd=1.5, pch = 1, col = "orange")
+lines(deps, accuracy[3,], type = "b", lwd=1.5, pch = 1, col = "red")
+lines(deps, accuracy[4,], type = "b", lwd=1.5, pch = 1, col = "blue")
+legend("bottomright", legend=c("QUILT-regular", "QUILT-mspbwt", "QUILT-zilong", "GLIMPSE"), col=c("black", "orange", "red", "blue"), pch = 1, lwd = 1.5, cex = 1.1, xjust = 0, yjust = 1, bty = "n")
+
+plot(1, col = "transparent", xlim = c(0, 0.5), ylim = c(0, 1.0), ylab = "Aggregated R2 within each MAF bin",  xlab = "Minor Allele Frequency")
+nd <- length(deps)
+for(i in 1:nd) {
+    d <- accuracy_by_af[[i]]
+    y <- unlist(ifelse(sapply(d$regular, is.null), NA, d$regular))
+    lines(as.numeric(d$bin), y, type = "b", lwd = i/nd * 2.5, pch = 1, col = "black")
+    y <- unlist(ifelse(sapply(d$mspbwt, is.null), NA, d$mspbwt))
+    lines(as.numeric(d$bin), y, type = "b", lwd = i/nd * 2.5, pch = 1, col = "orange")
+    y <- unlist(ifelse(sapply(d$zilong, is.null), NA, d$zilong))
+    lines(as.numeric(d$bin), y, type = "b", lwd = i/nd * 2.5, pch = 1, col = "red")
+    y <- unlist(ifelse(sapply(d$glimpse, is.null), NA, d$glimpse))
+    lines(as.numeric(d$bin), y, type = "b", lwd = i/nd * 2.5, pch = 1, col = "blue")
+}
+legend("bottomright", legend=paste0(deps, "x"), lwd = (1:nd) * 2.5 / nd, bty = "n")
+
+dev.off()
