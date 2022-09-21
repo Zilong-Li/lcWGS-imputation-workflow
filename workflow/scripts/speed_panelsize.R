82c6aa069372af02b5bfe0fad72fc10604b92cfb
Zilong-Li <zimusen94@gmail.com>
2022-09-21 12:48:46 +0100

    update speed plots

--- workflow/scripts/speed_panelsize.R
+++ workflow/scripts/speed_panelsize.R
@@ -23,7 +23,8 @@ dl.zilong <- lapply(snakemake@input[["zilong"]], read.table)
 dl.glimpse <- lapply(snakemake@input[["glimpse"]], read.table)
 saveRDS(list(regular = dl.regular, mspbwt = dl.mspbwt, zilong = dl.zilong, glimpse = dl.glimpse), snakemake@output[["rds"]])
 
-times <- cbind(gnutime(dl.regular), gnutime(dl.mspbwt), gnutime(dl.zilong), gnutime(dl.glimpse))
+## times <- cbind(gnutime(dl.regular), gnutime(dl.mspbwt), gnutime(dl.zilong), gnutime(dl.glimpse))
+times <- cbind(gnutime(dl.regular), gnutime(dl.mspbwt), gnutime(dl.glimpse))
 rams <- cbind(gunram(dl.regular), gunram(dl.mspbwt), gunram(dl.zilong), gunram(dl.glimpse))
 
 
@@ -35,8 +36,8 @@ pdf(snakemake@output[["pdf"]], w=12, h=6)
 par(mfrow=c(1,2))
 plot(groups, times[,1], type = "b", lwd=1.0, pch = 1, col = mycols[1], ylab = "Total Time in seconds for the chromosome", xlab = "Reference panel size", ylim=c(min(times)*0.9, max(times)*1.1))
 lines(groups, times[,2], type = "b", lwd=1.0, pch = 1, col = mycols[2])
-lines(groups, times[,3], type = "b", lwd=1.0, pch = 1, col = mycols[3])
-lines(groups, times[,4], type = "b", lwd=1.0, pch = 1, col = mycols[4])
+## lines(groups, times[,3], type = "b", lwd=1.0, pch = 1, col = mycols[3])
+lines(groups, times[,3], type = "b", lwd=1.0, pch = 1, col = mycols[4])
 legend("topleft", legend=c("QUILT-regular", "QUILT-mspbwt", "QUILT-zilong", "GLIMPSE"), col=mycols, pch = 1, lwd = 1.5, cex = 1.1, xjust = 0, yjust = 1, bty = "n")
 
 plot(groups, rams[,1], type = "b", lwd=1.0, pch = 1, col = mycols[1], ylab = "Maximum RAM in MBs for the chromosome", xlab = "Reference panel size", ylim=c(min(rams)*0.9, max(rams)*1.1))
